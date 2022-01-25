5c78e81adda92103f61e4c950050b55636ee9732
José Carvalho <z.m.f.carvalho@gmail.com>
2022-01-25 20:08:09 +0000

    Almost done int8

--- interrogações/int8.sql
+++ interrogações/int8.sql
@@ -1,16 +1,28 @@
-
---Average de lucro das vendas mensais
+-- Average de lucro das vendas mensais
 
 .mode columns
 .headers on
 .nullvalue NULL 
 
+-- Agrega o funcionario com as horas que trabalhou e a sua especialidade
+DROP VIEW IF EXISTS FUNCS;
+CREATE VIEW FUNCS AS
+SELECT FUNCIONARIO.ID_FUNCIONARIO AS ID,
+INCREMENTO_REPARACAO.HORAS AS HORAS_TRABALHO,
+FUNCIONARIO.ID_ESPECIALIDADE
+FROM FUNCIONARIO
+NATURAL JOIN SALARIO_MENSAL
+NATURAL JOIN INCREMENTO_REPARACAO;
 
-SELECT * FROM funcionario;
+-- Lucro final depois de descontado a comissao dos trabalhadores
+DROP VIEW IF EXISTS LIQ;
+CREATE VIEW LIQ AS
+SELECT SUM(CUSTO) - SUM(HORAS_TRABALHO*ESPECIALIDADE.PRECO_HORA) AS "Lucro liquido" FROM ESPECIALIDADE NATURAL JOIN FUNCS JOIN REPARACAO;
 
+-- Seleciona o lucro por mes considerando os gastos mensais com comissoes totais dos trabalhadores especializados da empresa e vendas
+-- Calculando preco por hora com as horas trabalhadas mensais
 
-
-/*SELECT CASE strftime('%m', DATA_SERVICO)
+SELECT CASE strftime('%m', DATA_SERVICO)
 when '01' then 'Janeiro'
 when '02' then 'Fevereiro' 
 when '03' then 'Marco' 
@@ -24,5 +36,6 @@ when '10' then 'Outubro'
 when '11' then 'Novembro' 
 when '12' then 'Dezembro' 
 else '' end AS "Mes",
-AVG(LUCRO) as "Lucro"
-FROM VENDA GROUP BY "Mes";*/
+LUCRO + "Lucro Liquido" as "lucro reparacoes + vendas" FROM VENDA JOIN LIQ GROUP BY "Mes";
+
+
