4f21dbac326adc66916cdff30cead5c2e2133ff7
Eduardo Correia <up201909914@fc.up.pt>
2022-01-25 23:17:04 +0000

    Trigger 2 done

--- gatilhos/gatilho2_adiciona.sql
+++ gatilhos/gatilho2_adiciona.sql
@@ -4,8 +4,13 @@
 
 PRAGMA foreign_keys = ON;
 
+
+DROP TRIGGER IF EXISTS CHECK_SALARIO_MENSAL;
+DROP TRIGGER IF EXISTS CHECK_DATE;
+
+--Cada funcionario so pode ter um salario num determinado mes. Logo,
 --Ao adicionar um registo ao salario mensal verifica se esse funcionario ja nao tem salario nesse mes.
-CREATE TRIGGER IF NOT EXISTS checkSalarioMensal
+CREATE TRIGGER IF NOT EXISTS CHECK_DATE
     BEFORE INSERT ON SALARIO_MENSAL
     FOR EACH ROW
     WHEN EXISTS(SELECT ID_FUNCIONARIO,ANO,MES 
@@ -15,5 +20,16 @@ CREATE TRIGGER IF NOT EXISTS checkSalarioMensal
                     AND SALARIO_MENSAL.MES = NEW.MES 
                     AND SALARIO_MENSAL.ANO = NEW.ANO)
     BEGIN
-        SELECT RAISE(Abort,"ja existe um funcionario com salario nessa data ou o funcionario nao existe");
+        SELECT RAISE(Abort,"ja existe um funcionario com salario nessa data");
+    END;
+
+
+CREATE TRIGGER IF NOT EXISTS CHECK_SALARIO_MENSAL
+    BEFORE INSERT ON SALARIO_MENSAL
+    FOR EACH ROW
+    WHEN (SELECT FUNCIONARIO.SALARIO_BASE 
+        FROM FUNCIONARIO 
+        WHERE FUNCIONARIO.ID_FUNCIONARIO = NEW.ID_FUNCIONARIO) > NEW.SALARIO
+    BEGIN
+        SELECT RAISE(Abort,"o salario inserido é invalido");
     END;
--- gatilhos/gatilho2_verifica.sql
+++ gatilhos/gatilho2_verifica.sql
@@ -4,7 +4,7 @@
 
 PRAGMA foreign_keys = ON;
 
-INSERT INTO SALARIO_MENSAL values (25,10, 1003.1, 2020, 15);
+INSERT INTO SALARIO_MENSAL values (25,10, 2003.1, 2020, 15);
 
 .print ''
 .print 'Foi introduzido o seguinte salario mensal:'
@@ -15,7 +15,7 @@ FROM SALARIO_MENSAL
 WHERE ID_SALARIO_MENSAL = 25;
 
 .print ''
-.print 'Vamos tentar adicionar outro registo com a mesma data e funcionario a tabela:'
+.print 'Vamos tentar adicionar outro registo com a mesma data e funcionario á tabela:'
 .print ''
 
 INSERT INTO SALARIO_MENSAL values (26,10, 2303.5, 2020, 15);
@@ -27,6 +27,21 @@ INSERT INTO SALARIO_MENSAL values (26,10, 2303.5, 2020, 15);
 SELECT ID_SALARIO_MENSAL,ID_FUNCIONARIO,SALARIO, MES, ANO, SALARIO
 FROM SALARIO_MENSAL
 WHERE ID_SALARIO_MENSAL = 26;
+
+.print ''
+.print 'Vamos agora tentar inserir um registo com um salario mensal inferior ao salario base:'
+.print ''
+
+INSERT INTO SALARIO_MENSAL values (27,12, 10.5, 2019, 14);
+
+SELECT ID_SALARIO_MENSAL,ID_FUNCIONARIO,SALARIO, MES, ANO, SALARIO
+FROM SALARIO_MENSAL
+WHERE ID_SALARIO_MENSAL = 27;
+
+.print ''
+.print 'Como o valor do salario mensal era inferior ao do salario base, nao foi inserido:'
+.print ''
+
 
 DELETE 
 FROM SALARIO_MENSAL
